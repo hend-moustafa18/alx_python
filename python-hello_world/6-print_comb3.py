911a9cc04983afbd35f2c3530f985e54416ca4ab
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-11-28 16:19:15 +0200

    new

---
+++ python-hello_world/6-print_comb3.py
@@ -0,0 +1,6 @@
+for i in range(0, 10):
+    for n in range(i + 1, 10):
+        if i == 8 and n == 9:
+            print("{}{}".format(i, n))
+        else:
+            print("{}{}".format(i, n), end=", ")
