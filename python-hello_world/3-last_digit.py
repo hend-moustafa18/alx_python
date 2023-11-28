3256d6082095c67e5a4187527661f612e2fa6e14
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-11-28 13:39:54 +0200

    new

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -3,9 +3,11 @@ import random
 number = random.randint(-10000, 10000)
 lastdigit = int(repr(number)[-1])
 # YOUR CODE HERE
+if number < 0:
+     lastdigit = lastdigit * -1
 if lastdigit > 5:
     print("Last digit of", number, "is", lastdigit, "and is greater than 5") 
 elif lastdigit == 0:
       print("Last digit of", number, "is", lastdigit, "and is 0") 
 else: 
-    print("Last digit of", number, "is", lastdigit, "and is less than 6 and not 0") 
+    print("Last digit of", number, "is", lastdigit, "and is less than 6 and not 0")
