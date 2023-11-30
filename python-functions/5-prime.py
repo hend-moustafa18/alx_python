d69bbbadf04711fc936b8f8b42fd173a775d0a60
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-11-30 13:44:47 +0200

    new

---
+++ python-functions/5-prime.py
@@ -0,0 +1,12 @@
+def is_prime(number):
+    if number <= 1:
+        return False
+
+    i = 2
+    while i < number:
+        if number % i == 0:
+            return False
+        i += 1
+
+    return True
+print(is_prime(15))
