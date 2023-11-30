a8bad0e71b891c82c41fcb8d927402386c7619d2
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-11-30 13:04:26 +0200

    new

--- python-functions/4-fibonacci.py
+++ python-functions/4-fibonacci.py
@@ -1,1 +1,9 @@
-def fibonacci_sequence(n)
+def fibonacci_sequence(n):
+    series = []
+    a, b = 0, 1
+
+    while len(series) < n:
+        series.append(a)
+        a, b = b, a + b
+
+    return series
