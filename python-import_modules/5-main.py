d62efaba1a6f0370629c1692f6b0bad4d0531f31
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-07 00:35:09 +0200

    new

--- python-import_modules/5-main.py
+++ python-import_modules/5-main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-        raise_exception_msg = __import__('5-raise_exception_msg').raise_exception_msg
+    raise_exception_msg = __import__('5-raise_exception_msg').raise_exception_msg
 try:
-    raise_exception_msg("C is fun")
+    message = "C is fun"
+    raise_exception_msg(message)
 except NameError as ne:
-    print(ne)
-    
+    print(message)
