a39340ad9fd066ad1222e6370e3891a2b09e7744
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-07 00:32:23 +0200

    new

---
+++ python-import_modules/5-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+        raise_exception_msg = __import__('5-raise_exception_msg').raise_exception_msg
+try:
+    raise_exception_msg("C is fun")
+except NameError as ne:
+    print(ne)
+    
