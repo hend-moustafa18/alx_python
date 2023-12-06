d01be3a9f9d3ca50355abd4fac931e35708b899e
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-06 19:51:41 +0200

    new

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,14 +1,13 @@
-import sys
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
 
-# Get the number of arguments
-num_args = len(sys.argv) - 1
-
-# Print the number of arguments
-if num_args == 0:
-    print("Number of argument(s): .")
-else:
-    print(f"Number of argument{'s' if num_args > 1 else ''}: {num_args}")
-
-# Print the list of arguments
-for i, arg in enumerate(sys.argv[1:]):
-    print(f"{i + 1}: {arg}")
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i in range(count):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
