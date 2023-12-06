b0b15399746e6a3236b98192aa477b37deec5b19
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-06 18:36:37 +0200

    new

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
-if ["__name__"] == ["__main__"]:
+if "__name__" == "__main__":
     from add_0 import add
-    ["a = 1"]
-    ["b = 2"]
+    a = 1
+    b = 2
+    c= add(a, b)
     print("{} + {b = {c}".format(1, 2, add(a,b)))
-
