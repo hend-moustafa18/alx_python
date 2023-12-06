2746794bdee657e6379a790ffc962cefdae9bd93
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-07 00:19:52 +0200

    NEW

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -1,12 +1,8 @@
 def safe_print_division(a, b):
-    a = 12
-    b = 2
-try:
-    result = a / b
-except ZeroDivisionError:
-    print("Inside result: Error: division by zero")
-    return None
-finally:
-    print("Inside result: {}".format(result))
-    
-return result
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+        return result
