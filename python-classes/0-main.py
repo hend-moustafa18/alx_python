4efe45badc61266408c4927803c20c3ffc9a6db1
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-12 18:26:51 +0200

    new

--- python-classes/0-main.py
+++ python-classes/0-main.py
@@ -10,16 +10,22 @@ if __name__ == "__main__":
         - Attempt to access the 'size' and '__size' attributes, catching and printing any exceptions.
     """
   Square = __import__('0-square').Square
-
+# Create a square object
 my_square = Square(3)
+
+# Print the type of the square object
 print(type(my_square))
+
+# Print the dictionary of the square object
 print(my_square.__dict__)
 
+# Try to access the size attribute directly
 try:
     print(my_square.size)
 except Exception as e:
     print(e)
 
+# Try to access the size attribute indirectly
 try:
     print(my_square.__size)
 except Exception as e:
