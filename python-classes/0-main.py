078aed3905dd9844c540111267c50acf6973926f
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-12 17:44:54 +0200

    new

--- python-classes/0-main.py
+++ python-classes/0-main.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
-if __name__ == "__main__": # define the class
+"""
+This script demonstrates the usage of the Square class.
+
+It creates an instance of the Square class, initializes it with a size, and showcases accessing attributes.
+
+Usage:
+- Import the Square class from the '0-square' module.
+- Create an instance of Square with a specified size.
+- Print the type of the instance and its attributes.
+- Attempt to access the 'size' and '__size' attributes, catching and printing any exceptions.
+"""
+if __name__ == "__main__": 
   Square = __import__('0-square').Square
-# define the class
+
 my_square = Square(3)
 print(type(my_square))
 print(my_square.__dict__)
-# define the class
+
 try:
     print(my_square.size)
 except Exception as e:
     print(e)
-# define the class
+
 try:
     print(my_square.__size)
 except Exception as e:
