bfd65c53008534ef2f1f923928007392b8a79df6
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-12 16:55:34 +0200

    new

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,7 @@
+# define the class
 class Square:
     def __init__(self, size):
+        """Initialize Square with size.
+            size (int): The size of the new square.
+        """
         self.__size = size
