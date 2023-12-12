5888e6dc4148e6183df7025cc25c25b5dd6cd02f
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-12 17:01:13 +0200

    new

--- python-classes/0-main.py
+++ python-classes/0-main.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-if __name__ == "__main__": 
+if __name__ == "__main__": # define the class
   Square = __import__('0-square').Square
-
+# define the class
 my_square = Square(3)
 print(type(my_square))
 print(my_square.__dict__)
-
+# define the class
 try:
     print(my_square.size)
 except Exception as e:
     print(e)
-
+# define the class
 try:
     print(my_square.__size)
 except Exception as e:
     print(e)
-    
