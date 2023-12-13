08c4d72a3132cf66893179d8c476070a02ffd18a
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-13 11:58:49 +0200

    new

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,21 +1,6 @@
-#!/usr/bin/python3
-# define the class
-class Square:
-    """
-    This class represents a square.
-
-    Attributes:
-        __size (int): The size of the square.
-
-    Methods:
-        __init__(self, size): Initializes a new instance of the Square class.
-    """
-
+"""Define a Square Class"""
+class Square():
+    """Define a private attribute size"""
     def __init__(self, size):
-         """
-        Initializes a new instance of the Square class.
-
-        Args:
-            size (int): The size of the square.
-        """
-         self.__size = size
+        self.__size = size
+        
