162eee4c259fb262be0e871dd25278ed3daa86ce
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-13 12:24:52 +0200

    new

---
+++ python-classes/3-main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":  
+ Square = __import__('3-square').Square
+
+my_square = Square(89)
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+my_square.size = 3
+print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+
+try:
+    my_square.size = "5 feet"
+    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
+except Exception as e:
+    print(e)
+    
