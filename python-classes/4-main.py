196440ab0ded2117678440eaa4cbdcee120bbbba
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-13 12:43:35 +0200

    new

---
+++ python-classes/4-main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+if __name__ == "__main__":  
+ Square = __import__('4-square').Square
+
+my_square = Square(3)
+my_square.my_print()
+
+print("--")
+
+my_square.size = 10
+my_square.my_print()
+
+print("--")
+
+my_square.size = 0
+my_square.my_print()
+
+print("--")
