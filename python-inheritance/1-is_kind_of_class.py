c355e1a616dcada91688f0e55e6e9bf77c65571d
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-14 17:04:33 +0200

    new

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,6 @@
+"""Define is kind of class Function"""
+def is_kind_of_class(obj, a_class):
+    """Check The Object is an instance of, 
+    or if the object is an instance 
+    of a class that inherited from """
+    return isinstance(obj,a_class)
