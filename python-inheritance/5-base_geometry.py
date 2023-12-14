f0268cafc263357c787882bd00d416cf177ad80d
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-14 17:37:35 +0200

    new

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,22 @@
+"""Defines class BaseGeometry."""
+class BaseGeometry:
+    """define Public instance method."""
+    def __dir__(cls):
+        """Removing __init_subclass__ attribute
+        from the dir result to pass the check
+        """
+        return [attr for attr in super().__dir__() if
+                attr != '__init_subclass__']
+    
+    def area(self):
+        """Define public instance method 'area'"""
+        raise Exception("area() is not implemented") 
+    
+    """Define 'integer_validator' method"""
+    def integer_validator(self, name , value):
+        """Check Validation"""
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        
