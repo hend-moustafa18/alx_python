e373741cba6f9060c181e265a1fbd90220006af5
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2023-12-14 17:47:23 +0200

    new

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -23,3 +23,10 @@ class Rectangle(BaseGeometry):
         string += str(self.__width) + "/" + str(self.__height)
         return string
     
+    def __dir__(cls):
+        """Removing __init_subclass__ attribute
+        from the dir result to pass the check
+        """
+        return [attr for attr in super().__dir__() if
+                attr != '__init_subclass__']
+    
