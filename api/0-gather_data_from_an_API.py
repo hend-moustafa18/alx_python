e546cd82d5ad68d842b3781ad7dc7b9bb9aaa605
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2024-02-28 01:53:30 +0200

    new

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -21,9 +21,9 @@ def fetch_employee_data(employee_id):
 
     return employee_data, todo_data
 
-def display_todo_progress(employee_name, completed_tasks, total_tasks, completed_task_titles):
+def display_todo_progress(employee_name, completed_tasks, total_tasks, task_titles):
     print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
-    for title in completed_task_titles:
+    for title in task_titles:
         print(f"\t{title[:50]:<50}")
 
 def main():
@@ -44,9 +44,9 @@ def main():
     employee_name = employee_data.get("name")
     total_tasks = len(todo_data)
     completed_tasks = sum(1 for task in todo_data if task.get("completed"))
-    completed_task_titles = [task.get('title') for task in todo_data if task.get("completed")]
+    task_titles = [task.get('title') for task in todo_data]
 
-    display_todo_progress(employee_name, completed_tasks, total_tasks, completed_task_titles)
+    display_todo_progress(employee_name, completed_tasks, total_tasks, task_titles)
 
 if __name__ == "__main__":
     main()
