b5b5cd3aff0b25d68d7acce35ed0272894b248c2
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2024-02-29 04:33:54 +0200

    new

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -4,8 +4,8 @@ import requests
 import sys
 
 def getData(id):
-    usersur1 = f"https://jsonplaceholder.typicode.com/users/{id}"
-    todour1 = f"{usersur1}/todos"
+    usersur1 = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    todour1 = "{}/todos".format(usersur1)
 
     request1 = requests.get(usersur1)
     result = request1.json()
@@ -15,7 +15,7 @@ def getData(id):
     request2 = requests.get(todour1)
     tasks = request2.json()
 
-    csv_filename = f"{userid}.csv"
+    csv_filename = "{}.csv".format(userid)
 
     with open(csv_filename, "w", newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
@@ -23,16 +23,13 @@ def getData(id):
             writer.writerow([userid, username, task['completed'], task['title']])
 
     # Check if the number of tasks in CSV is equal to the number of tasks obtained from the API
-    if os.path.exists(csv_filename):
-        with open(csv_filename, 'r') as f:
-            csv_reader = csv.reader(f)
-            num_tasks_in_csv = sum(1 for _ in csv_reader)
-            if num_tasks_in_csv == len(tasks):
-                print("Number of tasks in CSV: OK")
-            else:
-                print("Number of tasks in CSV: Incorrect")
-    else:
-        print(f"Error: File {csv_filename} not found.")
+    with open(csv_filename, 'r') as f:
+        csv_reader = csv.reader(f)
+        num_tasks_in_csv = sum(1 for _ in csv_reader)
+        if num_tasks_in_csv == len(tasks):
+            print("Number of tasks in CSV: OK")
+        else:
+            print("Number of tasks in CSV: Incorrect")
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
