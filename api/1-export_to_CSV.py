a8a1dbb46823e90ea6c667988fa3354e321e1a3d
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2024-02-29 03:25:03 +0200

    new

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -2,28 +2,27 @@ import csv
 import requests
 import sys
 
-def getData(id):
-    usersur1 = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-    todour1 = "{}/todos".format(usersur1)
+def get_data(user_id):
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    todos_url = f"{user_url}/todos"
 
-    request1 = requests.get(usersur1)
-    result = request1.json()
-    userid = result['id']
-    username = result['username']
+    response_user = requests.get(user_url)
+    user_data = response_user.json()
+    username = user_data['username']
 
-    request2 = requests.get(todour1)
-    tasks = request2.json()
+    response_todos = requests.get(todos_url)
+    tasks = response_todos.json()
 
-
-    with open("{}.csv".format(userid), "w" , newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting = csv.QUOTE_ALL)
+    with open(f"{user_id}.csv", "w", newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
         for task in tasks:
-            writer.writerow([userid, username, task['completed'], task['title']])
-    
+            writer.writerow([user_id, username, task['completed'], task['title']])
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        id = sys.argv[1]
+        user_id = int(sys.argv[1])
     else:
-        id = 1
-    getData(id)
-    
+        print("Usage: python script.py <user_id>")
+        sys.exit(1)
+
+    get_data(user_id)
