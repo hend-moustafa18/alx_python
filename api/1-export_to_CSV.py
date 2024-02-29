099e9d5837f25f3bd2723bf1424bf578cfd96b7e
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2024-02-29 04:39:01 +0200

    new

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -14,7 +14,7 @@ def getData(id):
     request2 = requests.get(todour1)
     tasks = request2.json()
 
-    csv_filename = "{}.csv".format(userid)
+    csv_filename = "1.csv"  # Use a fixed filename
 
     with open(csv_filename, "w", newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
