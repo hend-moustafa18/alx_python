bc547a351a7aa5285617e43fd3e9e732b71726f5
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2024-02-29 05:09:39 +0200

    new

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,52 +1,41 @@
 #!/usr/bin/python3
-'''
-    Retrieve user information (ID and username) using the given employee_id.
-    
-    Args:
-        employee_id (int): The ID of the employee.
-        
-    Returns:
-        Tuple[int, str]: A tuple containing the user's ID and username.
-'''
+"""
+Script that, using this REST API, for a given employee ID, returns
+information about his/her TODO list progress
+and export data in the JSON format.
+"""
+
 import json
 import requests
-import sys
+from sys import argv
 
-def get_user_info(employee_id):
-    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
-    if user_response.status_code == 200:
-        user_data = user_response.json()
-        return user_data['id'], user_data['username']
-    else:
-        return None, None
-
-def export_to_json(employee_id, username, todo_data):
-    json_data = {str(employee_id): [{"task": task['title'], "completed": task['completed'], "username": username} for task in todo_data]}
-    json_filename = f"{employee_id}.json"
-
-    with open(json_filename, "w") as jsonfile:
-        json.dump(json_data, jsonfile, indent=2)
-
-    print(f"Data has been exported to {json_filename}")
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: python3 script.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-
-    user_id, username = get_user_info(employee_id)
-
-    if user_id is None:
-        print(f"User with ID {employee_id} not found.")
-        sys.exit(1)
-
-    response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
-    tasks = response.json()
-
-    export_to_json(employee_id, username, tasks)
 
 if __name__ == "__main__":
-    main()
 
+    sessionReq = requests.Session()
+
+    idEmp = argv[1]
+    idURL = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(idEmp)
+    nameURL = 'https://jsonplaceholder.typicode.com/users/{}'.format(idEmp)
+
+    employee = sessionReq.get(idURL)
+    employeeName = sessionReq.get(nameURL)
+
+    json_req = employee.json()
+    usr = employeeName.json()['username']
+
+    totalTasks = []
+    updateUser = {}
+
+    for all_Emp in json_req:
+        totalTasks.append(
+            {
+                "task": all_Emp.get('title'),
+                "completed": all_Emp.get('completed'),
+                "username": usr,
+            })
+    updateUser[idEmp] = totalTasks
+
+    file_Json = idEmp + ".json"
+    with open(file_Json, 'w') as f:
+        json.dump(updateUser, f)
