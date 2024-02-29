abb04dfe817e6cf4d6b073dedb70d372ef8859ca
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2024-02-29 05:01:10 +0200

    new

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -12,36 +12,40 @@ import json
 import requests
 import sys
 
-def getData(id):
-    users_url = f"https://jsonplaceholder.typicode.com/users/{id}"
-    todos_url = f"{users_url}/todos"
+def get_user_info(employee_id):
+    user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+    if user_response.status_code == 200:
+        user_data = user_response.json()
+        return user_data['id'], user_data['username']
+    else:
+        return None, None
 
-    user_response = requests.get(users_url)
-    user_data = user_response.json()
-
-    if not user_data:
-        print(f"User with ID {id} not found.")
-        return
-
-    username = user_data['username']
-
-    todos_response = requests.get(todos_url)
-    tasks = todos_response.json()
-
-    json_data = {str(id): [{"task": task['title'], "completed": task['completed'], "username": username} for task in tasks]}
-
-    json_filename = f"{id}.json"
+def export_to_json(employee_id, username, todo_data):
+    json_data = {str(employee_id): [{"task": task['title'], "completed": task['completed'], "username": username} for task in todo_data]}
+    json_filename = f"{employee_id}.json"
 
     with open(json_filename, "w") as jsonfile:
         json.dump(json_data, jsonfile, indent=2)
 
     print(f"Data has been exported to {json_filename}")
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        id = int(sys.argv[1])
-    else:
+def main():
+    if len(sys.argv) != 2:
         print("Usage: python3 script.py <employee_id>")
         sys.exit(1)
 
-    getData(id)
+    employee_id = int(sys.argv[1])
+
+    user_id, username = get_user_info(employee_id)
+
+    if user_id is None:
+        print(f"User with ID {employee_id} not found.")
+        sys.exit(1)
+
+    response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    tasks = response.json()
+
+    export_to_json(employee_id, username, tasks)
+
+if __name__ == "__main__":
+    main()
