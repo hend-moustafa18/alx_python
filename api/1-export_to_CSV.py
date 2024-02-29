dc59f8a52ab6dfec34fd8b3b72abc4d31647f65c
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2024-02-29 14:22:20 +0200

    new

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -15,21 +15,25 @@ def getData(id):
 
     csv_filename = f"{id}.csv"  # Use a dynamic filename based on USER_ID
 
+    # Check if the CSV file already exists, delete it
+    if os.path.exists(csv_filename):
+        os.remove(csv_filename)
+
     with open(csv_filename, "w", newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])  # Add header
         for task in tasks:
             writer.writerow([id, user_data['username'], task['completed'], task['title']])
 
-    # Print some debug information
-    print(f"Number of tasks obtained from API: {len(tasks)}")
-
     # Check if the number of tasks in CSV is equal to the number of tasks obtained from the API
     with open(csv_filename, 'r') as f:
         csv_reader = csv.reader(f)
         next(csv_reader)  # Skip the header
         num_tasks_in_csv = sum(1 for _ in csv_reader)
-        print(f"Number of tasks written to CSV: {num_tasks_in_csv}")
+
+    # Print some debug information
+    print(f"Number of tasks obtained from API: {len(tasks)}")
+    print(f"Number of tasks written to CSV: {num_tasks_in_csv}")
 
     if num_tasks_in_csv == len(tasks):
         print("Number of tasks in CSV: OK")
@@ -41,10 +45,5 @@ if __name__ == "__main__":
         id = int(sys.argv[1])
     else:
         id = 1
-
-    # Check if the CSV file already exists, delete it
-    csv_filename = f"{id}.csv"
-    if os.path.exists(csv_filename):
-        os.remove(csv_filename)
 
     getData(id)
