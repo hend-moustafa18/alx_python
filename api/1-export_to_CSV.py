20dc0ba70e4e25b4010edc079e5d48085c6c8415
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2024-02-29 04:45:56 +0200

    new

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -3,36 +3,39 @@ import requests
 import sys
 
 def getData(id):
-    users_url = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-    todos_url = "{}/todos".format(users_url)
+    usersur1 = "https://jsonplaceholder.typicode.com/users/{}".format(id)
+    todour1 = "{}/todos".format(usersur1)
 
-    user_response = requests.get(users_url)
-    user_data = user_response.json()
+    request1 = requests.get(usersur1)
+    result = request1.json()
+    userid = result['id']
+    username = result['username']
 
-    if not user_data:
-        print(f"User with ID {id} not found.")
-        return
+    request2 = requests.get(todour1)
+    tasks = request2.json()
 
-    username = user_data['username']
-
-    todos_response = requests.get(todos_url)
-    tasks = todos_response.json()
-
-    csv_filename = "{}.csv".format(id)  # Use a dynamic filename
+    csv_filename = "{}.csv".format(userid)  # Use a dynamic filename
 
     with open(csv_filename, "w", newline='') as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])  # Add header
         for task in tasks:
-            writer.writerow([id, username, task['completed'], task['title']])
+            writer.writerow([userid, username, task['completed'], task['title']])
 
-    print(f"Data has been exported to {csv_filename}")
+    # Check if the number of tasks in CSV is equal to the number of tasks obtained from the API
+    with open(csv_filename, 'r') as f:
+        csv_reader = csv.reader(f)
+        next(csv_reader)  # Skip the header
+        num_tasks_in_csv = sum(1 for _ in csv_reader)
+
+    if num_tasks_in_csv == len(tasks):
+        print("Number of tasks in CSV: OK")
+    else:
+        print("Number of tasks in CSV: Incorrect")
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         id = int(sys.argv[1])
     else:
-        print("Usage: python3 script.py <employee_id>")
-        sys.exit(1)
-
+        id = 1
     getData(id)
