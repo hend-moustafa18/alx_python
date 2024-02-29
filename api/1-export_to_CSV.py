2e0ad75ed91381dd1e48fee7ff391beb7c38b16f
Hend-Moustafa18 <hend.moustafa18@icloud.com>
2024-02-29 03:57:06 +0200

    new

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -14,16 +14,15 @@ def getData(id):
     request2 = requests.get(todour1)
     tasks = request2.json()
 
-
     with open("{}.csv".format(userid), "w" , newline='') as csvfile:
         writer = csv.writer(csvfile, quoting = csv.QUOTE_ALL)
         for task in tasks:
             writer.writerow([userid, username, task['completed'], task['title']])
-    
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         id = int(sys.argv[1])
     else:
         id = 1
-    getData(id)
     
+    getData(id)
